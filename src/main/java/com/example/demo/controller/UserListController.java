7d5667c528d41e2b726cf952a311871c16e2d7c4
Dakiee <ldavaanyam@gmail.com>
2022-07-10 16:52:49 +0800

    8 done

--- src/main/java/com/example/demo/controller/UserListController.java
+++ src/main/java/com/example/demo/controller/UserListController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserListController {
-    
+    // Display user list
+    @GetMapping("/list")
+    public String getUserList() {
+        // Display userlist
+        return "user/list";
+    }
 }
